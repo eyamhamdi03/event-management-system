3aef6c10d7c0ff907653595672ce7b48df1df6a8
meliuos <mootez.souilem@insat.ucar.tn>
2025-05-31 13:46:31 +0100

    Re setup front

---
+++ frontend/src/integrations/tanstack-query/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export function LayoutAddition() {
+  return <ReactQueryDevtools buttonPosition="bottom-right" />
+}
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { ReactNode } from 'react'
+
+const queryClient = new QueryClient()
+
+interface TanStackQueryLayoutProps {
+  children: ReactNode
+}
+
+export default function TanStackQueryLayout({ children }: TanStackQueryLayoutProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
